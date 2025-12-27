4a9e48826be1ea4e69389cd373899e2b2041fe16
yash <yashgouravkar@gmail.com>
2025-12-27 13:47:32 +0530

    ss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import { ProfileForm } from '~/components/linkedin/profile-form';
 
 export default function Home() {
   return (
     <main className="flex min-h-[calc(100dvh-4rem)] w-full items-start justify-center px-4 py-8 lg:items-center lg:py-12">
-      <ProfileForm />
+      <Suspense>
+        <ProfileForm />
+      </Suspense>
     </main>
   );
 }
